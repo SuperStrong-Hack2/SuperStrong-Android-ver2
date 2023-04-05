781a6484cc89622d9e119d9c5e03042cc22fd8e2
yunzi <iyunji323@gmail.com>
2023-04-05 14:21:23 +0900

    [52th commit] Payment 송금 findview에서 binding 방식으로 변경 완료

--- app/src/main/java/com/superstrong/android/view/PaymentActivity.kt
+++ app/src/main/java/com/superstrong/android/view/PaymentActivity.kt
@@ -7,33 +7,45 @@ import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.superstrong.android.R
+import com.superstrong.android.databinding.ActivityPaymentBinding
 
 class PaymentActivity : AppCompatActivity() {
 
-//    // 전역 변수로 바인딩 객체 선언
-//    private var mBinding: PaymentActivityMainBinding? = null
-//    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
-//    private val binding get() = mBinding!!
+    private lateinit var binding: ActivityPaymentBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment)
 
+        binding = ActivityPaymentBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener(MyNavigationListener(this))
 
-        val NextButton : Button = findViewById(R.id.next_button);
-        // findViewById 대신에 ViewBinding을 사용한다
+//        val NextButton : Button = findViewById(R.id.next_button);
+//        // findViewById 대신에 ViewBinding을 사용한다
+//
+//        val BackButton : ImageView = findViewById(R.id.btn_back);
+//
+//        NextButton.setOnClickListener {
+//            val intent = Intent(this, PaymentActivity2::class.java)
+//            startActivity(intent)
+//        }
+//
+//        BackButton.setOnClickListener {
+//            val intent = Intent(this, PaymentActivity2::class.java)
+//            startActivity(intent)
+//        }
 
-        val BackButton : ImageView = findViewById(R.id.btn_back);
-
-        NextButton.setOnClickListener {
-            val intent = Intent(this, PaymentActivity2::class.java)
+        // 툴바 뒤로가기 버튼
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, WalletActivity::class.java)
             startActivity(intent)
+            finish()
         }
 
-        // TODO: toolbar 연결
-        BackButton.setOnClickListener {
+        binding.nextButton.setOnClickListener {
             val intent = Intent(this, PaymentActivity2::class.java)
             startActivity(intent)
         }
--- app/src/main/java/com/superstrong/android/view/PaymentActivity2.kt
+++ app/src/main/java/com/superstrong/android/view/PaymentActivity2.kt
@@ -7,26 +7,32 @@ import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.superstrong.android.R
+import com.superstrong.android.databinding.ActivityPayment2Binding
+import com.superstrong.android.databinding.ActivityPaymentBinding
 
 class PaymentActivity2 : AppCompatActivity() {
 
-//    // 전역 변수로 바인딩 객체 선언
-//    private var mBinding: PaymentActivityMainBinding? = null
-//    // 매번 null 체크를 할 필요 없이 편의성을 위해 바인딩 변수 재 선언
-//    private val binding get() = mBinding!!
+    private lateinit var binding: ActivityPayment2Binding
 
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_payment2)
 
+        binding = ActivityPayment2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         val bottomNavigationView: BottomNavigationView = findViewById(R.id.navigationView)
         bottomNavigationView.setOnNavigationItemSelectedListener(MyNavigationListener(this))
 
-        val NextButton : Button = findViewById(R.id.next_button);
-        // findViewById 대신에 ViewBinding을 사용한다
+        // 툴바 뒤로가기 버튼
+        binding.btnBack.setOnClickListener {
+            val intent = Intent(this, PaymentActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
 
-        NextButton.setOnClickListener {
+        binding.nextButton.setOnClickListener {
             val intent = Intent(this, PaymentActivity3::class.java)
             startActivity(intent)
         }
