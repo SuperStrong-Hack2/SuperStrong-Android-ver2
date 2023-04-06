f1e554b6ebd671a6794a6138773f60eec3f0f85e
조현성 <gustjd1478@korea.ac.kr>
2023-04-06 11:26:41 +0900

    [65th commit] 움직이는 너구리

--- app/src/main/java/com/superstrong/android/data/RetrofitInstance.kt
+++ app/src/main/java/com/superstrong/android/data/RetrofitInstance.kt
@@ -3,20 +3,20 @@ package com.superstrong.android.data
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
-class RetrofitInstance {
-    var retrofitInstance: RetrofitInstance? = null
-    var retrofitService: RetrofitServiceApi? = null
+//class RetrofitInstance {
+  //  var retrofitInstance: RetrofitInstance? = null
+ //   var retrofitService: RetrofitServiceApi? = null
 
     // BaseUrl등록
-    private val BASE_URL = "http://localhost:8080/"
+//    private val BASE_URL = "http://localhost:8080/"
 
-    private fun RetrofitInstance() {
-        val retrofit = Retrofit.Builder()
-            .baseUrl(BASE_URL) // Json을 변환해줄 Gson변환기 등록
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-        retrofitService = retrofit.create(RetrofitServiceApi::class.java)
-    }
+  //  private fun RetrofitInstance() {
+  //      val retrofit = Retrofit.Builder()
+  //          .baseUrl(BASE_URL) // Json을 변환해줄 Gson변환기 등록
+  //          .addConverterFactory(GsonConverterFactory.create())
+  //          .build()
+   //     retrofitService = retrofit.create(RetrofitServiceApi::class.java)
+   // }
 
 //    fun getRetrofitInstance(): RetrofitInstance? { // 싱글톤
 //        if (retrofitInstance == null) {
@@ -25,7 +25,7 @@ class RetrofitInstance {
 //        return retrofitInstance
 //    }
 
-    fun getRetrofitService(): RetrofitServiceApi? {
-        return retrofitService
-    }
-}
+//    fun getRetrofitService(): RetrofitServiceApi? {
+//        return retrofitService
+//    }
+//}
--- app/src/main/java/com/superstrong/android/view/SplashActivity.kt
+++ app/src/main/java/com/superstrong/android/view/SplashActivity.kt
@@ -4,19 +4,21 @@ package com.superstrong.android.view
 import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.superstrong.android.R
-
+import com.bumptech.glide.Glide
 class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
-
+        val imageView = findViewById<ImageView>(R.id.imageView)
+        Glide.with(this).load(R.drawable.movelog).into(imageView)
         // 5초 후 LoginActivity로 이동하고 현재 Activity를 종료
         Handler().postDelayed({
             startActivity(Intent(this, LoginActivity::class.java))
             finish()
-        }, 5000)
+        }, 9000)
     }
 }
