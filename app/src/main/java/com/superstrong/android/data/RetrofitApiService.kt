e5b73f7453d34c02daca711601eec125b60ea5b2
Sanghoon <victory743@naver.com>
2023-04-07 09:31:43 +0900

    [71th commit] 회원 가입 요청 보내기

--- app/src/main/java/com/superstrong/android/data/RetrofitApiService.kt
+++ app/src/main/java/com/superstrong/android/data/RetrofitApiService.kt
@@ -4,9 +4,11 @@ import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.google.gson.JsonObject
 import retrofit2.Call
+import retrofit2.Response
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
+import retrofit2.http.Headers
 import retrofit2.http.POST
 
 public interface BackendApiService {
@@ -16,9 +18,18 @@ public interface BackendApiService {
     @POST("api/sendinput")
     fun payment(@Body payInfo: PayInfo ): Call<String>
 
+
+    @POST("api/register")
+    @Headers("Content-Type: application/json")
+    suspend fun signUp(@Body body: SignUpRequestBody): Response<SignUpResponseBody>
+
+    @POST("api/auth")
+    @Headers("Content-Type: application/json")
+    fun emailAuth(@Body body: authCode): Call<UserData>
+
 }
 object RetrofitInstance {
-    private const val BASE_URL = "https://6510-222-236-64-196.jp.ngrok.io"
+    private const val BASE_URL = "https://6510-222-36-64-196.jp.ngrok.io"
     private val gson: Gson = GsonBuilder().create()
     private val retrofit = Retrofit.Builder()
         .baseUrl(BASE_URL)
