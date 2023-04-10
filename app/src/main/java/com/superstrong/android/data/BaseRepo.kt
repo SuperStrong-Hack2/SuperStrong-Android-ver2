f24e57952c3feef54f338699ea5600ee2f60f546
Sanghoon <victory743@naver.com>
2023-04-10 11:15:38 +0900

    [80th commit] baserepo 추가

---
+++ app/src/main/java/com/superstrong/android/data/BaseRepo.kt
@@ -0,0 +1,51 @@
+package com.superstrong.android.data
+
+import android.util.Log
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+
+
+sealed class Resource<T>(
+    val data :T? = null,
+    val message: String? = null){
+
+    // We'll wrap our data in this 'Success'
+    // class in case of success response from api
+    class Success<T>(data: T) : Resource<T>(data = data)
+
+    // We'll pass error message wrapped in this 'Error'
+    // class to the UI in case of failure response
+    class Error<T>( errorMessage: String) : Resource<T>( message = errorMessage)
+
+    // We'll just pass object of this Loading
+    // class, just before making an api call
+
+    class Loading<T> : Resource<T>()
+}
+
+abstract class BaseRepo() {
+    suspend fun <T> safeApiCall(apiToBeCalled: suspend () -> Response<T>): Resource<T> {
+
+        // Returning api response
+        // wrapped in Resource class
+        return withContext(Dispatchers.IO) {
+            try {
+                val response: Response<T> = apiToBeCalled()
+
+                if (response.isSuccessful) {
+                    Resource.Success(data = response.body()!!)
+                } else {
+                    Log.d("safeApicall", "not successful")
+                    Resource.Error( errorMessage =  "Something went wrong")
+                }
+
+            } catch (e: Exception) {
+                // Returning 'Something went wrong' in case
+                // of unknown error wrapped in Resource.Error
+                Log.d("safeApicall", "something wrong")
+                Resource.Error(errorMessage = "Something went wrong")
+            }
+        }
+    }
+}
