1f64cca17da1bb159b0ab6c2111bf0528806a101
yunzi <iyunji323@gmail.com>
2023-04-11 15:42:10 +0900

    [97th commit] 암호화 수정중 (privatekey, serverkey)

--- app/src/main/java/com/superstrong/android/viewmodel/Cipher.kt
+++ app/src/main/java/com/superstrong/android/viewmodel/Cipher.kt
@@ -1,6 +1,10 @@
 package com.superstrong.android.viewmodel
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.util.Base64
+import com.google.gson.JsonObject
+import retrofit2.Callback
 import java.nio.charset.Charset
 import java.security.spec.AlgorithmParameterSpec
 import javax.crypto.Cipher
@@ -8,8 +12,20 @@ import javax.crypto.spec.IvParameterSpec
 import javax.crypto.spec.SecretKeySpec
 object AES256Util {
     //키값 32바이트: AES256(24: AES192, 16: AES128)
-    var secretKey = "01234567890123456789012345678901"
+
+    private lateinit var context: Context
+    var serverKey = "01234567890123456789012345678901"
+    private lateinit var sharedPref: SharedPreferences
+    private lateinit var secretKey: String
+//    val sharedPref = getSharedPreferences("strong", Context.MODE_PRIVATE)
+//    var secretKey = sharedPref.getString("key","")
     var ivBytes = "0123456789012345".toByteArray()
+
+    fun init(context: Context) {
+        this.context = context
+        this.sharedPref = context.getSharedPreferences("strong", Context.MODE_PRIVATE)
+        this.secretKey = sharedPref.getString("key","") ?: ""
+    }
 
     //AES256 암호화
     fun aesEncode(str: String): String {
